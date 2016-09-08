53f9f7ef8cdf56dd91ad973e61a02b6febf268b4
Dave Tang <davetingpongtang@gmail.com>
2016-09-08 14:23:38 +0800

    Variants causing Kabuki syndrome

---
+++ variant/README.md
@@ -0,0 +1,5 @@
+Machine learning on variants
+============================
+
+* Known pathogenic variants for [Kabuki syndrome](https://en.wikipedia.org/wiki/Kabuki_syndrome) from the work by [Ng et al. in Nature Genetics 2010](http://www.ncbi.nlm.nih.gov/pubmed/20711175); variants available in [Supplementary Table 3](http://www.nature.com/ng/journal/v42/n9/extref/ng.646-S1.pdf).
+
---
+++ variant/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# replace with locations of the binary and chain file
+liftOver kabuki_hg18.bed hg18ToHg19.over.chain kabuki_hg19.bed unmapped.txt
+
+# bases are on the negative strand
+cat kabuki_hg19.bed | perl -nle '@a=split; if($a[3] =~ /([ACGT])\d+([ACGT])/){ $r = $1; $r =~ tr/ACGT/TGCA/; $a = $2; $a =~ tr/ACGT/TGCA/; print join("\t", @a[0..2], $r, $a) }' > kabuki_hg19.tsv
+
+# create VCF file
+~/github/learning_vcf_file/script/create_vcf.pl ~/genome/hg19/hg19.fa kabuki_hg19.tsv > kabuki_hg19.vcf
+
+# dbNSFP
+wget -c ftp://dbnsfp:dbnsfp@dbnsfp.softgenetics.com/dbNSFPv3.2a.zip
+unzip dbNSFPv3.2a.zip
+java search_dbNSFP32a -i kabuki_hg19.vcf -o kabuki_hg19_dbnsfp.out -v hg19
+
