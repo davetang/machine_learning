41363461e259641111acb692217130f62c9648fa
Dave Tang <me@davetang.org>
2016-09-14 10:48:19 +0800

    False positives and negatives

---
+++ variant/random_forest/README.md
@@ -0,0 +1,42 @@
+Random Forest classifier
+========================
+
+See my [Random Forest repository](https://github.com/davetang/learning_random_forest) for introductory information. See [analysis.R](https://github.com/davetang/machine_learning/blob/master/variant/random_forest/analysis.R) for the analysis.
+
+# False positives
+
+~~~~{.r}
+# false positives, listed as positive but really is negative
+data[c(7766,14528,19722,20147,23042,28612),c('CHR','Nuc.Pos','REF.Nuc','ALT.Nuc','X.RS.ID','True.Label')]
+# CHR   Nuc.Pos REF.Nuc ALT.Nuc    X.RS.ID True.Label
+# 7766   14  62016431       A       T rs35561533         -1
+# 14528  19  11348960       G       A rs12609039         -1
+# 19722   1 246930564       G       C     rs7779         -1
+# 20147   1  44456013       G       C rs35904809         -1
+# 23042  22  45944576       T       A  rs1802787         -1
+# 28612   4 166403424       T       A rs34516004         -1
+~~~~
+
+# False negatives
+
+~~~~{.r}
+# false negatives, i.e. listed as negative but really is positive
+data[c(4015,6867,20187,22798,34844,36297,39089),c('CHR','Nuc.Pos','REF.Nuc','ALT.Nuc','X.RS.ID','True.Label')]
+# CHR  Nuc.Pos REF.Nuc ALT.Nuc    X.RS.ID True.Label
+# 4015   11 76853783       T       C  rs1052030          1
+# 6867   13 52544715       C       A          ?          1
+# 20187   1 45481018       G       A          ?          1
+# 22798  22 36661906       A       G rs73885319          1
+# 34844   8 21976710       T       C  rs7014851          1
+# 36297   9 34649442       A       G  rs2070074          1
+# 39089   X 31496398       T       C  rs1800279          1
+~~~~
+
+* [rs1052030 is listed as benign](http://www.ncbi.nlm.nih.gov/clinvar/variation/43258/)
+* [rs73885319 is listed only as a risk factor](http://www.ncbi.nlm.nih.gov/clinvar/variation/6080/)
+* [rs7014851 is listed as uncertain significance](http://www.ncbi.nlm.nih.gov/clinvar/variation/7330/)
+* [rs2070074 has conflicting information](http://www.ncbi.nlm.nih.gov/clinvar/variation/3613/)
+* [rs1800279 has conflicting information](http://www.ncbi.nlm.nih.gov/clinvar/variation/11269/)
+
+It is not certain whether the above variants are really false negatives, as none of them are listed as only pathogenic in ClinVar.
+
--- variant/random_forest/analysis.R
+++ variant/random_forest/analysis.R
@@ -1,6 +1,6 @@
 setwd("~/github/machine_learning/variant/random_forest/")
 
-data <- read.csv("../DataS1/ToolScores/humvar_tool_scores.csv")
+data <- read.csv("../DataS1/ToolScores/humvar_tool_scores.csv", stringsAsFactors = FALSE)
 head(data)
 
 features <- c('MutationTaster','MutationAssessor','PolyPhen2','CADD','SIFT','LRT','FatHMM.U','GERP..','PhyloP')
@@ -25,13 +25,89 @@ library(ROCR)
 # in the first row, all trees voted for class 0, which is benign
 head(r$votes)
 
+# cases with NaN were omitted
+# create vector to store cases that were used
 my_pred_vector <- as.numeric(names(r$predicted))
+
+# votes for 1
 pred <- prediction(r$votes[,2], as.numeric(data_subset$True.Label[my_pred_vector]))
 perf <- performance(pred,"tpr","fpr")
 plot(perf)
 
-# Area under the curve
+# area under the curve
 auc <- performance(pred, measure = "auc")
 auc@y.values
-
 legend('bottomright', legend = paste("AUC = ", auc@y.values))
+
+# matrix with votes and true label
+votes_and_truth <- cbind(r$votes, data$True.Label[my_pred_vector])
+votes_and_truth <- as.data.frame(votes_and_truth)
+names(votes_and_truth) <- c('Negative','Positive','True')
+
+# dplyr magic
+filter(votes_and_truth, Positive>0.50) %>%
+  select(True) %>%
+  group_by(True) %>%
+  tally(True)
+# A tibble: 2 x 2
+#    True     n
+#   <dbl> <dbl>
+# 1    -1 -2169
+# 2     1 14783
+
+filter(votes_and_truth, Negative>0.50) %>%
+  select(True) %>%
+  group_by(True) %>%
+  tally(True)
+# A tibble: 2 x 2
+#     True      n
+#    <dbl>  <dbl>
+#  1    -1 -12454
+#  2     1   2364
+
+# find absolutely misclassified examples
+# classified as positive but is really negative
+# i.e. false positive
+filter(votes_and_truth, Positive==1, True==-1)
+
+# need row information
+votes_truth_row <- votes_and_truth
+votes_truth_row$Row <- rownames(votes_and_truth)
+
+# see http://stackoverflow.com/questions/21618423/extract-a-dplyr-tbl-column-as-a-vector
+filter(votes_truth_row, Positive==1, True==-1) %>%
+  select(Row) %>%
+  collect %>% .[[1]]
+
+data[c(7766,14528,19722,20147,23042,28612),c('CHR','Nuc.Pos','REF.Nuc','ALT.Nuc','X.RS.ID','True.Label')]
+# CHR   Nuc.Pos REF.Nuc ALT.Nuc    X.RS.ID True.Label
+# 7766   14  62016431       A       T rs35561533         -1
+# 14528  19  11348960       G       A rs12609039         -1
+# 19722   1 246930564       G       C     rs7779         -1
+# 20147   1  44456013       G       C rs35904809         -1
+# 23042  22  45944576       T       A  rs1802787         -1
+# 28612   4 166403424       T       A rs34516004         -1
+
+# false negative
+filter(votes_truth_row, Negative>0.99, True==1) %>%
+  select(Row) %>%
+  collect %>% .[[1]]
+data[c(4015,6867,20187,22798,34844,36297,39089),c('CHR','Nuc.Pos','REF.Nuc','ALT.Nuc','X.RS.ID','True.Label')]
+# CHR  Nuc.Pos REF.Nuc ALT.Nuc    X.RS.ID True.Label
+# 4015   11 76853783       T       C  rs1052030          1
+# 6867   13 52544715       C       A          ?          1
+# 20187   1 45481018       G       A          ?          1
+# 22798  22 36661906       A       G rs73885319          1
+# 34844   8 21976710       T       C  rs7014851          1
+# 36297   9 34649442       A       G  rs2070074          1
+# 39089   X 31496398       T       C  rs1800279          1
+
+# prediction
+# values from data_subset[10,]
+values <- c(0.0004286186,-0.205,0.003,10,0.83,0.0312836,0.36,-1.58,-0.462)
+blah <- t(data.frame(x = values))
+colnames(blah) <- features
+rownames(blah) <- 10
+
+predict(r, blah)
+
