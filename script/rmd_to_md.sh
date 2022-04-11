5148e18554d684f49b8a7b0994839d927df5ea9f
Dave Tang <davetingpongtang@gmail.com>
2022-04-11 12:45:20 +0900

    Optional outfile arg

--- script/rmd_to_md.sh
+++ script/rmd_to_md.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 num_param=1
 
 usage(){
-   echo "Usage: $0 <file.Rmd>"
+   echo "Usage: $0 <file.Rmd> [outfile.md]"
    exit 1
 }
 
-if [[ $# -ne ${num_param} ]]; then
+if [[ $# -lt ${num_param} ]]; then
    usage
 fi
 
@@ -17,6 +17,11 @@ infile=$1
 if [[ ! -e ${infile} ]]; then
   >&2 echo ${infile} does not exist
   exit 1
+fi
+
+outfile=README.md
+if [[ $# -ge 2 ]]; then
+   outfile=$2
 fi
 
 check_depend (){
@@ -40,8 +45,6 @@ SECONDS=0
 
 >&2 printf "[ %s %s ] Start job\n\n" $(now)
 
-img_dir=$(cat ${infile} | grep "knitr::opts_chunk\$set(fig.path" | cut -f2 -d '=' | sed 's/[")]//g;s/\/\n$//')
-
 r_version=4.1.3
 docker_image=davetang/r_build:${r_version}
 package_dir=${HOME}/r_packages_${r_version}
@@ -58,7 +61,7 @@ docker run \
    -e USERID=$(id -u) \
    -e GROUPID=$(id -g) \
    ${docker_image} \
-   Rscript -e "rmarkdown::render('${infile}', output_file = 'README.md')"
+   Rscript -e "rmarkdown::render('${infile}', output_file = '${outfile}')"
 
 >&2 printf "\n[ %s %s ] Work complete\n" $(now)
 
