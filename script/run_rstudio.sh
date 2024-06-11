499f519b8e8e397baec2a966e69b406f0a9fe3aa
Dave Tang <davetingpongtang@gmail.com>
2024-06-11 10:28:44 +0900

    Start RStudio Server

---
+++ script/run_rstudio.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VER=4.4.0
+IMG=davetang/r_tensorflow:${VER}
+CONTAINER=rstudio_server_r_tensorflow
+PORT=8883
+RLIB=${HOME}/r_packages_${VER}
+
+if [[ ! -d ${RLIB} ]]; then
+   mkdir ${RLIB}
+fi
+
+docker run \
+   --name ${CONTAINER} \
+   -d \
+   --rm \
+   -p ${PORT}:8787 \
+   -v ${RLIB}:/packages \
+   -v ${HOME}/github/:/home/rstudio/work \
+   -v ${HOME}/gitlab/:/home/rstudio/gitlab \
+   -v ${HOME}/analysis/:/analysis \
+   -e PASSWORD=password \
+   -e USERID=$(id -u) \
+   -e GROUPID=$(id -g) \
+   ${IMG}
+
+>&2 echo ${CONTAINER} listening on port ${PORT}
+
+exit 0
